81b014945a63b19dd0e939c5c8142fc52e8e8822
imnabeelkhan <nabeelkhan5666@gmail.com>
2022-06-08 03:26:04 +0500

    Updates

---
+++ precache-manifest.8dbd83c16f263b636f88610550b3742a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2d057165e32d88a8859429535cfef5",
+    "url": "/Landing-Page/index.html"
+  },
+  {
+    "revision": "1415979b7fe8ec482cb2",
+    "url": "/Landing-Page/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "3936489ff8202a5aae5a",
+    "url": "/Landing-Page/static/css/main.ac70efda.chunk.css"
+  },
+  {
+    "revision": "1415979b7fe8ec482cb2",
+    "url": "/Landing-Page/static/js/2.a5fd47a4.chunk.js"
+  },
+  {
+    "revision": "7a3bb90361613bc4c6a5ff31d13b8af0",
+    "url": "/Landing-Page/static/js/2.a5fd47a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3936489ff8202a5aae5a",
+    "url": "/Landing-Page/static/js/main.26c848a7.chunk.js"
+  },
+  {
+    "revision": "d471b876031922369ffb",
+    "url": "/Landing-Page/static/js/runtime-main.c03eb6d9.js"
+  },
+  {
+    "revision": "85e2808ae2a1d0e4fbecfb0ed7f02d44",
+    "url": "/Landing-Page/static/media/cta-illustration.85e2808a.svg"
+  },
+  {
+    "revision": "82692e7b5047a62f3b895009509b176e",
+    "url": "/Landing-Page/static/media/d1.82692e7b.png"
+  },
+  {
+    "revision": "467dff266e0a80f8a93bbdad4266078f",
+    "url": "/Landing-Page/static/media/d2.467dff26.png"
+  },
+  {
+    "revision": "dac9684d12c016f963132c69d9c94bab",
+    "url": "/Landing-Page/static/media/d3.dac9684d.png"
+  },
+  {
+    "revision": "7eb920ca0b4ea593a1129dd3ed372b8c",
+    "url": "/Landing-Page/static/media/hero-back-illustration.7eb920ca.svg"
+  },
+  {
+    "revision": "aa0f14cb87e54d157f8fa6dd8b3308f4",
+    "url": "/Landing-Page/static/media/hero-top-illustration.aa0f14cb.svg"
+  },
+  {
+    "revision": "7ad0f1af4b9d9a8755245ec8be17d4b5",
+    "url": "/Landing-Page/static/media/l1.7ad0f1af.png"
+  },
+  {
+    "revision": "cd803903700bb93f7d76c86a216b9948",
+    "url": "/Landing-Page/static/media/lap.cd803903.jpg"
+  }
+]);
